4e1684bc9bc2f8f5363da015d3281c687062839f
NefiseDemir <nfshtndmr@hotmail.com>
2024-12-18 00:41:41 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo";
+import InlineComponent from "./components/InlineComponent";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Todo /> */}
+      <InlineComponent />
+    </div>
+  );
+}
+
+export default App;
